9a3e3c118897aba38aa284b20ac3d3d370118203
RafaelLanaDS <rafael.lanads@outlook.com>
2025-11-12 22:19:29 -0300

    commit

--- Escritorio/index.php
+++ Escritorio/index.php
@@ -6,7 +6,7 @@ $escritorio = new Escritorio();
 $gaveta1 = new Gaveta();
 $gaveta1->adicionarItem(new Documento("Contrato", "Contrato do cliente X", "2024-05-10"));
 $gaveta1->adicionarItem(new Objeto("Caneta", "Caneta azul", 0.02));
-$gaveta1->adicionarItem(new Objeto("Borracha", 0.10));
+
 
 $gaveta2 = new Gaveta();
 $gaveta2->adicionarItem(new Pasta("Projetos", "Pasta dos projetos", "TI", "Consultoria", "Projetos Academicos"));
